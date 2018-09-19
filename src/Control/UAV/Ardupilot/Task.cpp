bce82cb73be6b137a2b1238cccc164bc97ebcf29
Miguel Nunes <spacemig@gmail.com>
2018-09-19 17:49:47 +0100

    UAV/Ardupilot: revert old merge

--- src/Control/UAV/Ardupilot/Task.cpp
+++ src/Control/UAV/Ardupilot/Task.cpp
@@ -116,10 +116,6 @@ namespace Control
       //! %Task arguments.
       struct Arguments
       {
-        //! Use TCP or UDP
-        bool tcp_or_udp;
-        //! Only receive mode
-        bool receive_only;
         //! Communications timeout
         uint8_t comm_timeout;
         //! Use Ardupilot's waypoint tracker
@@ -198,8 +194,6 @@ namespace Control
         IMC::ControlLoops m_controllps;
         //! TCP socket
         Network::TCPSocket* m_TCP_sock;
-        //! UDP socket
-        Network::UDPSocket* m_UDP_sock;
         //! System ID
         uint8_t m_sysid;
         //! Last received position
@@ -251,7 +245,6 @@ namespace Control
         Task(const std::string& name, Tasks::Context& ctx):
           Tasks::Task(name, ctx),
           m_TCP_sock(NULL),
-          m_UDP_sock(NULL),
           m_sysid(1),
           m_lat(0.0),
           m_lon(0.0),
@@ -280,14 +273,6 @@ namespace Control
           m_last_wp(0),
           m_land(false)
         {
-          param("Use TCP (or UDP)", m_args.tcp_or_udp)
-          .defaultValue("true")
-          .description("Use TCP or UDP for comms");
-
-          param("Receive Only", m_args.receive_only)
-          .defaultValue("false")
-          .description("To only receive mode");
-
           param("Communications Timeout", m_args.comm_timeout)
           .minimumValue("1")
           .maximumValue("60")
@@ -490,7 +475,6 @@ namespace Control
         onResourceRelease(void)
         {
           Memory::clear(m_TCP_sock);
-          Memory::clear(m_UDP_sock);
         }
 
         void
@@ -519,25 +503,11 @@ namespace Control
         void
         openConnection(void)
         {
-          if (m_args.receive_only)
-            inf("Receive only mode");
-
           try
           {
-            if (m_args.tcp_or_udp)
-            {
-              m_TCP_sock = new TCPSocket;
-              m_TCP_sock->connect(m_args.TCP_addr, m_args.TCP_port);
-              m_TCP_sock->setNoDelay(true);
-            }
-            else
-            {
-              m_UDP_sock = new UDPSocket;
-              m_UDP_sock->bind(m_args.UDP_listen_port, Address::Any, false);
-
-              inf(DTR("Ardupilot UDP connected"));
-            }
-
+            m_TCP_sock = new TCPSocket;
+            m_TCP_sock->connect(m_args.TCP_addr, m_args.TCP_port);
+            m_TCP_sock->setNoDelay(true);
             setupRate(m_args.trate);
             inf(DTR("Ardupilot interface initialized"));
 
@@ -550,7 +520,6 @@ namespace Control
           catch (...)
           {
             Memory::clear(m_TCP_sock);
-            Memory::clear(m_UDP_sock);
             war(DTR("Connection failed, retrying..."));
             setEntityState(IMC::EntityState::ESTA_NORMAL, Status::CODE_COM_ERROR);
           }
@@ -1507,7 +1476,7 @@ namespace Control
           while (!stopping())
           {
             // Handle data
-            if (m_TCP_sock || m_UDP_sock)
+            if (m_TCP_sock)
             {
               handleArdupilotData();
             }
@@ -1545,26 +1514,16 @@ namespace Control
           if (m_TCP_sock != NULL)
             return Poll::poll(*m_TCP_sock, timeout);
 
-          if (m_UDP_sock != NULL)
-            return Poll::poll(*m_UDP_sock, timeout);
-
           return false;
         }
 
         int
         sendData(uint8_t* bfr, int size)
         {
-          if (m_args.receive_only)
-            return 0;
           if (m_TCP_sock)
           {
             trace("Sending something");
             return m_TCP_sock->write((char*)bfr, size);
-          }
-          else if (m_UDP_sock)
-          {
-            trace("Sending something");
-            return m_UDP_sock->write(bfr, size, m_args.UDP_addr, m_args.UDP_port);
           }
           return 0;
         }
@@ -1572,34 +1531,20 @@ namespace Control
         int
         receiveData(uint8_t* buf, size_t blen)
         {
-          if (m_TCP_sock || m_UDP_sock)
+          if (m_TCP_sock)
           {
             try
             {
-              if (m_TCP_sock)
-                return m_TCP_sock->read(buf, blen);
-              if (m_UDP_sock)
-                return m_UDP_sock->read(buf, blen, &m_args.UDP_addr, &m_args.UDP_listen_port);
+              return m_TCP_sock->read(buf, blen);
             }
             catch (std::runtime_error& e)
             {
               err("%s", e.what());
               war(DTR("Connection lost, retrying..."));
               Memory::clear(m_TCP_sock);
-              Memory::clear(m_UDP_sock);
 
-              if (m_args.tcp_or_udp)
-              {
-                m_TCP_sock = new TCPSocket;
-                m_TCP_sock->connect(m_args.TCP_addr, m_args.TCP_port);
-                m_TCP_sock->setNoDelay(true);
-              }
-              else
-              {
-                m_UDP_sock = new UDPSocket;
-                m_UDP_sock->bind(m_args.UDP_listen_port, Address::Any, false);
-              }
-
+              m_TCP_sock = new Network::TCPSocket;
+              m_TCP_sock->connect(m_args.TCP_addr, m_args.TCP_port);
               return 0;
             }
           }
@@ -1619,7 +1564,6 @@ namespace Control
             counter++;
 
             int n = receiveData(m_buf, sizeof(m_buf));
-            inf("Received bytes %d", n);
             if (n < 0)
             {
               debug("Receive error");
